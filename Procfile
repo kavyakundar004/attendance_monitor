0f696a980700d377f7d12c083dcf4952e430d07d
Kavya kundar <kavyakundar004@gmail.com>
2026-05-24 09:29:21 +0530

    Move deployment files to root for Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: cd home/webapp/attendance_monitor && daphne attendance_monitor.asgi:application --port $PORT --bind 0.0.0.0
---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+# Move into the Django project directory
+cd home/webapp/attendance_monitor
+
+python manage.py collectstatic --no-input
+python manage.py migrate
